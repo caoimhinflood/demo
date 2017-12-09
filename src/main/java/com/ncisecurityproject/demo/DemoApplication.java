35bea3f538e41eb271c3b389253d31e41766695b
Caoimhin Flood <caoimhinflood@gmail.com>
2017-12-09 14:52:29 -0500

    Add updates to gradle and spring properties file

--- src/main/java/com/ncisecurityproject/demo/DemoApplication.java
+++ src/main/java/com/ncisecurityproject/demo/DemoApplication.java
@@ -3,10 +3,17 @@ package com.ncisecurityproject.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.jfrog.artifactory.client.Artifactory;
+import org.jfrog.artifactory.client.ArtifactoryClient;
+import org.jfrog.artifactory.client.model.RepoPath;
+
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+                
+        
 	}
 }
